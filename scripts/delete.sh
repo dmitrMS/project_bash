e26dc00f772cfd1c9991b5131aee4487a604802b
dmiryMS <dm.zhukov7@mail.ru>
2023-07-19 18:08:56 +0300

    v3

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 filename=$(find ./catalog/ -name $file*)
-# directory_name=$(find ./catalog/ -name $file)
-# echo "$directory_name"
+
 if [ -f $filename ]; then
+
     if basename $filename > /dev/null 2>&1 
     then
         rm $filename
@@ -10,6 +10,7 @@ if [ -f $filename ]; then
     else 
         echo "Неправильное название файла или директории"
     fi
+
 elif [ -d $filename ]; then
 rm -R $filename
 echo "ok"
